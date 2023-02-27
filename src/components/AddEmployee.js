8298c5680b24fb8d7dbe75475825b53bb6660a95
Yasi <113851370+YasiruBasura@users.noreply.github.com>
2023-02-27 23:43:20 +0530

    FrontEnd-Backend connect

--- src/components/AddEmployee.js
+++ src/components/AddEmployee.js
@@ -1,6 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import EmployeeService from '../services/EmployeeService';
 
 const AddEmployee = () => {
+
+const [employee, setEmployee] = useState({
+        id:"",
+        firstName:"",
+        lastName:"",
+        emailId:"",
+      //setting the initial values
+});
+
+const handleChange=(e) =>{
+  const value=e.target.value; //the value which are getting
+  setEmployee({...employee,[e.target.name]:value});   //form this the update in your input field the same will be updated to the state as well.
+}
+
+const saveEmployee=(e) =>{ //this function is nothing but taking the event as input parameter
+  e.preventDefault();       //this will disable the refreshing of the page
+  EmployeeService.saveEmployee(employee).then((response) =>{
+      console.log(response);
+
+  }).catch((Error)=>{
+    console.log(Error);
+  });
+};
+
+
+
   return (
   <div className='flex max-w-xl mx-auto shadow border-b'>
     <div className='px-8 py-8'>
@@ -15,7 +42,11 @@ const AddEmployee = () => {
         <label className="block text-gray-600 text-sm font-normal">
           First Name
         </label>
-      <input type="text" className="h-8 w-96 border mt-2 px-2 py-2">
+      <input type="text"
+      name='firstName'              //define the name of the tag
+      value={employee.firstName}   //define the value of the tag //state.firstName
+      onChange={(e)=>handleChange(e)}  //onchange of the input field here we are creating a method
+      className="h-8 w-96 border mt-2 px-2 py-2">
       </input>
       </div>
 
@@ -24,7 +55,11 @@ const AddEmployee = () => {
       <label className="block text-gray-600 text-sm font-normal">
         Last Name
       </label>
-      <input type="text" className="h-8 w-96 border mt-2 px-2 py-2">
+      <input type="text"
+      name='lastName'   //here the name of the curent element is lastName
+      value={employee.lastName}
+      onChange={(e)=>handleChange(e)}   //So here 'e' would be the the current element and e.target for the event.(here it will be the particular input) and e.target.name
+      className="h-8 w-96 border mt-2 px-2 py-2">
       </input>
       </div>
 
@@ -33,12 +68,16 @@ const AddEmployee = () => {
       <label className="block text-gray-600 text-sm font-normal">
         Email
       </label>
-      <input type={'email'} className="h-8 w-96 border mt-2 px-2 py-2">
+      <input type= "email" 
+       name='emailId'
+       value={employee.emailId}
+       onChange={(e)=>handleChange(e)}
+      className="h-8 w-96 border mt-2 px-2 py-2">
       </input>
       </div>
 
      <div className=' justify content-center  h-10 w-full  space-x-2 pt-4'>
-        <button className="rounded text-white font-semibold bg-green-400 py-2 px-2 hover:bg-green-700">
+        <button onClick={saveEmployee} className="rounded text-white font-semibold bg-green-400 py-2 px-2 hover:bg-green-700">
           Save
         </button>
         <button className="rounded text-white font-semibold bg-red-400 py-2 px-2 hover:bg-red-700">
---
+++ src/services/EmployeeService.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/employee"
+class EmployeeService{
+
+    saveEmployee(employee) { //take employee object as the input parameter
+    
+            return axios.post(EMPLOYEE_API_BASE_URL,employee)  //here we are passing the entire object to the localhost url 
+    
+    
+    }
+}
+
+export default new EmployeeService();
